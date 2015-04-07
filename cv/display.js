6c09f594e496bb49831cb9866ac1e471f80819f6
yiducn <sunsnowad@gmail.com>
2015-04-07 16:23:40 +0800

    add cv
    add cv

---
+++ cv/display.js
@@ -0,0 +1,20 @@
+/**
+ * Created by cnic on 2014/12/17.
+ */
+function showProject(){
+    $("#cv").hide();
+    $("#project").show();
+    $("#main").hide();
+}
+
+function showMain(){
+    $("#cv").hide();
+    $("#project").hide();
+    $("#main").show();
+}
+
+function showCV(){
+    $("#cv").show();
+    $("#project").hide();
+    $("#main").hide();
+}
