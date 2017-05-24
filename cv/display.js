77b32817f67a9b4f25901aaf3970964f1b37c433
yi du mac <duyi001@gmail.com>
2017-05-24 20:10:32 +0800

    update cv

--- cv/display.js
+++ cv/display.js
@@ -1,5 +1,5 @@
 /**
- * Created by cnic on 2014/12/17.
+ * Created by yidu on 2014/12/17.
  */
 function showProject(){
     $("#cv").hide();
@@ -17,4 +17,52 @@ function showCV(){
     $("#cv").show();
     $("#project").hide();
     $("#main").hide();
+}
+
+function displayOldProject(){
+  $(".oldproj").attr("class", "oldproj collapse in");
+  $("#labelHideOldProj").attr("class", "collapse in");
+  $("#labelDisplayOldProj").attr("class", "collapse out");
+}
+function hideOldProject(){
+  $(".oldproj").attr("class", "oldproj collapse out");
+  $("#labelDisplayOldProj").attr("class", "collapse in");
+  $("#labelHideOldProj").attr("class", "collapse out");
+}
+
+function displayOldRProject(){
+  $(".oldrproj").attr("class", "oldrproj collapse in");
+  $("#labelHideOldRProj").attr("class", "collapse in");
+  $("#labelDisplayOldRProj").attr("class", "collapse out");
+}
+function hideOldRProject(){
+  $(".oldrproj").attr("class", "oldrproj collapse out");
+  $("#labelDisplayOldRProj").attr("class", "collapse in");
+  $("#labelHideOldRProj").attr("class", "collapse out");
+}
+
+function displayOnlyNewPaper(){
+    $(".normalpaper").removeClass("collapse out");
+    $(".normalpaper").addClass("collapse in");
+    $(".classicpaper").removeClass("collapse in");
+    $(".classicpaper").addClass("collapse in");
+
+    $(".oldpaper").removeClass("collapse in");
+    $(".oldpaper").addClass("collapse out");
+}
+
+function displayOnlyClassicPaper(){
+  $(".normalpaper").removeClass("collapse in");
+  $(".normalpaper").addClass("collapse out");
+
+  $(".classicpaper").removeClass("collapse out");
+  $(".classicpaper").addClass("collapse in");
+
+}
+
+function displayAll(){
+  $(".normalpaper").removeClass("collapse out");
+  $(".normalpaper").addClass("collapse in");
+  $(".classicpaper").removeClass("collapse in");
+  $(".classicpaper").addClass("collapse in");
 }
